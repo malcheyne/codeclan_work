c618153297a8b607f1847f226a0b5c001279851c
malcheyne <92717988+malcheyne@users.noreply.github.com>
2021-11-11 15:43:02 +0000

    end of class

---
+++ Week_02/Day_4/3_relative_filepaths/no_project_example/scripts/reading_in_data_script.R
@@ -0,0 +1,7 @@
+library(tidyverse)
+
+#check what your working directory is
+getwd()
+
+#using tab navigate to the students.csv which is saved in the 'data' folder in the same folder as this .R file
+read_csv("")
---
+++ Week_02/Day_4/Functions/subset_function.R
@@ -0,0 +1,7 @@
+
+subset_function <- function(x) {
+  
+  subset <- x[1:5]
+  
+  print(subset)
+}
